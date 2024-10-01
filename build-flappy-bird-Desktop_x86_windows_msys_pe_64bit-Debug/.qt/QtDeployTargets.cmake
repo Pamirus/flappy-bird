54153f4743f3b19d04c948e6e849eaaa346c6ef0
Pamirus <pamirfescioglu@gmail.com>
2024-10-01 11:10:03 +0300

    Refactoring the project files

--- build-flappy-bird-Desktop_x86_windows_msys_pe_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_flappy-bird_FILE C:/Users/pamir/Desktop/C++/flappy-bird-qt/build-flappy-bird-Desktop_x86_windows_msys_pe_64bit-Debug/flappy-bird.exe)
-set(__QT_DEPLOY_TARGET_flappy-bird_RUNTIME_DLLS C:/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
